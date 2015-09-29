0d26ea8ba8b9a4b5458dd640ce50396962acdbcd
BisongT <btangban@yahoo.co.uk>
2015-09-29 11:55:21 +1000

    Wordpress site

---
+++ local-config.php
@@ -0,0 +1,27 @@
+<?php 
+// Global DB config
+if (!defined('DB_NAME')) {
+	define('DB_NAME', 'bisongt');
+}
+if (!defined('DB_USER')) {
+	define('DB_USER', 'bisongt');
+}
+if (!defined('DB_PASSWORD')) {
+	define('DB_PASSWORD', 'Events@site');
+}
+if (!defined('DB_HOST')) {
+	define('DB_HOST', 'localhost');
+}
+
+/** Database Charset to use in creating database tables. */
+if (!defined('DB_CHARSET')) {
+	define('DB_CHARSET', 'utf8');
+}
+
+/** The Database Collate type. Don't change this if in doubt. */
+if (!defined('DB_COLLATE')) {
+	define('DB_COLLATE', '');
+}
+
+
+ ?>
