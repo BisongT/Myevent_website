88efe18313f7e2879861435e5eb0b72785148023
BisongT <btangban@yahoo.co.uk>
2015-10-07 18:00:29 +1000

    7/10/2015 changes

--- wp-content/themes/Basetheme/templates/footer.php
+++ wp-content/themes/Basetheme/templates/footer.php
@@ -1,5 +1,7 @@
 <footer class="content-info" role="contentinfo">
   <div class="container">
+     &copy2015 
     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
 </footer>
+
--- wp-content/themes/Basetheme/templates/head.php
+++ wp-content/themes/Basetheme/templates/head.php
@@ -2,5 +2,10 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="/skin.css">
+    <title>Welcome to Events Home</title>
   <?php wp_head(); ?>
 </head>
+
+   
