c193c412dcf2cd7fd8dfb5aaf6096ec29381537f
Brad Wong <display@SURGE-DISPLAYs-MacBook-Air-Laptop.local>
2015-10-08 08:51:46 +1000

    changes at the end of work 7/10/15

--- wp-content/themes/Basetheme/templates/header.php
+++ wp-content/themes/Basetheme/templates/header.php
@@ -1,16 +1,25 @@
+
 <header class="banner" role="banner">
+
+<div id="top">
+    <h1>LogoEvents</h1>
+ </div>
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+  <?php  /* <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?>Logo</a> */ ?>
+
     <nav class="navbar navbar-default col-xs-12 ">
-      <div class="navbar-header">
+
+    <?php  /*  ?> <div class="navbar-header">
         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#primary-navigation" aria-expanded="false">
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-      </div>
+      </div> <?php */ ?>
 
       <div class="collapse navbar-collapse " id="primary-navigation">
+
+
           <?php
               if (has_nav_menu('primary_navigation')) :
                 wp_nav_menu(['theme_location' => 'primary_navigation','depth'=> 7,'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav navbar-nav']);
