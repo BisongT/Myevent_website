9451af0f42a0f8a029cd623d781f403fdf15d12b
Brad Wong <display@SURGE-DISPLAYs-MacBook-Air-Laptop.local>
2015-10-21 13:18:23 +1000

    Creating a category for event website.

--- wp-content/themes/Basetheme/template-cat-eventpage.php
+++ wp-content/themes/Basetheme/template-cat-eventpage.php
@@ -1,37 +1,60 @@
 <?php
 /**
- * Template Name: Events page
+ * Template Name: Cat Event page
  */
 ?>
-<?php while (have_posts()) : the_post(); ?>
+
   <?php // class="container"><?php get_template_part('templates/page', 'header');?>
 
 <div class="container">
         <img src="http://onehdwallpaper.com/wp-content/uploads/2014/10/Easter-Desktop-Backgrounds.jpg">
-
+</div>
 <div class="container" style="text-align: center; margin-bottom: 0px;"><div><span><h2 class="intro"><strong>Our Events</strong></h2></span></div></div>
    <hr />
    <section class="container"><h3 class="intro_text">We will make your dreams come true</h3></section>
+
 <hr>
   
   <div class="container">
+
+
+  
+
+<?php //End taxonomy ?>
+
+<?php while (have_posts()) : the_post(); ?>
 <?php // WP_Query arguments
 
-	 //get_template_part('templates/post', 'event-loop'); 
 
-$terms = get_terms( 'events' );
-debug($terms);
- //if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
-     echo '<ul>';
-     foreach ( $terms as $term ) {
-       echo '<li>' . $term->name . '</li>';
-       
-     }
-     echo '</ul>';
-// }
+// WP_Query arguments
+$args = array (
+	'post_type'       => array( 'newsevents' ),
+    'events'          => get_field('taxonomy'),
+	
+);
 
 
-?>
+
+// The Query
+$query = new WP_Query( $args );
+
+// The Loop
+if ( $query->have_posts() ) {
+	while ( $query->have_posts() ) {
+		$query->the_post();
+		
+	 get_template_part('templates/post', 'event-loop'); 
+	   
+	}
+} 
+
+else {
+	echo 'no posts';
+}
+
+// Restore original Post Data
+wp_reset_postdata();
+ ?>
+
 <?php endwhile;?>
-
 </div>
