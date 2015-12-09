3c0631555bc138a0e1e318e5857b30f45ef954ef
BisongT <btangban@yahoo.co.uk>
2015-12-10 08:48:58 +1000

    Some changes to the event page.

--- wp-content/themes/Basetheme/template-occasion.php
+++ wp-content/themes/Basetheme/template-occasion.php
@@ -12,7 +12,7 @@
       $args = array('post_type' => 'occasions');
       $query = new WP_Query($args);
       while ($query -> have_posts()): $query -> the_post();
-
+      debug($args);
       get_template_part();
 
      ?>
