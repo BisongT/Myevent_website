0cfc6d60c9f72e584a5fc4f7b6c90486babf7793
BisongT <btangban@yahoo.co.uk>
2015-12-24 08:04:52 +1000

    Created an event page with  all my events that's some progress after a long while.

--- wp-content/themes/Basetheme/template-occasion.php
+++ wp-content/themes/Basetheme/template-occasion.php
@@ -10,7 +10,9 @@
 
 <div class="container">
     <?php
-      $args = array('post_type' => 'occasions');
+      $args = array(
+        'post_type' => array('occasions')
+      );
       $query = new WP_Query($args);
       while ($query -> have_posts()): $query -> the_post();
       //debug($args);
